54d2c5d2045949fe126d93054971a4365b4e7014
chaudhuree <chaudhuree00@gmail.com>
2022-06-08 02:20:15 +0600

    docs:lazy state initialization

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -2,16 +2,51 @@
 // http://localhost:3000/isolated/exercise/02.js
 
 import * as React from 'react'
+//
+// function Greeting({initialName = ''}) {
+//   // 🐨 initialize the state to the value from localStorage
+//   // 💰 window.localStorage.getItem('name') ?? initialName
+//   const [name, setName] = React.useState(
+//     window.localStorage.getItem('name') || initialName,
+//   )
+//
+//   // 🐨 Here's where you'll use `React.useEffect`.
+//   // The callback should set the `name` in localStorage.
+//   // 💰 window.localStorage.setItem('name', name)
+//   React.useEffect(() => window.localStorage.setItem('name', name))
+//   function handleChange(event) {
+//     setName(event.target.value)
+//   }
+//   return (
+//     <div>
+//       <form>
+//         <label htmlFor="name">Name: </label>
+//         <input value={name} onChange={handleChange} id="name" />
+//       </form>
+//       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+//     </div>
+//   )
+// }
+//
+// function App() {
+//   return <Greeting initialName={'sOhan'} />
+// }
+//
+// export default App
+
+//////////Lazy State Initialization/////////////
 
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+  const getInitialName = () =>
+    window.localStorage.getItem('name') || initialName
+  const [name, setName] = React.useState(getInitialName)
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
-
+  React.useEffect(() => window.localStorage.setItem('name', name))
   function handleChange(event) {
     setName(event.target.value)
   }
@@ -27,7 +62,7 @@ function Greeting({initialName = ''}) {
 }
 
 function App() {
-  return <Greeting initialName={'sOhan'}/>
+  return <Greeting initialName={'sOhan'} />
 }
 
 export default App
