0835c0ea0123cad9edc3f523ccec71de490ba3a1
chaudhuree <chaudhuree00@gmail.com>
2022-06-13 04:26:57 +0600

    docs: error bundary reset

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,6 +3,7 @@
 
 import * as React from 'react'
 import {fetchPokemon, PokemonDataView, PokemonForm, PokemonInfoFallback} from '../pokemon'
+import {ErrorBoundary} from 'react-error-boundary'
 
 //
 // import {
@@ -198,25 +199,168 @@ import {fetchPokemon, PokemonDataView, PokemonForm, PokemonInfoFallback} from '.
 
 //error boundary
 
+//
+// class ErrorBoundary extends React.Component {
+//   state = {error: null}
+//   static getDerivedStateFromError(error) {
+//     return {error}
+//   }
+//   render() {
+//     const {error} = this.state
+//     if (error) {
+//       return <this.props.FallbackComponent error={error} />
+//     }
+//
+//     return this.props.children
+//   }
+// }
+//
+// function PokemonInfo({pokemonName}) {
+//   const [state, setState] = React.useState({
+//     status: 'idle',
+//     pokemon: null,
+//     error: null,
+//   })
+//   const {status, pokemon, error} = state
+//
+//   React.useEffect(() => {
+//     if (!pokemonName) {
+//       return
+//     }
+//     setState({status: 'pending'})
+//     fetchPokemon(pokemonName).then(
+//       pokemon => {
+//         setState({status: 'resolved', pokemon})
+//       },
+//       error => {
+//         setState({status: 'rejected', error})
+//       },
+//     )
+//   }, [pokemonName])
+//
+//   if (status === 'idle') {
+//     return 'Submit a pokemon'
+//   } else if (status === 'pending') {
+//     return <PokemonInfoFallback name={pokemonName} />
+//   } else if (status === 'rejected') {
+//     // this will be handled by an error boundary
+//     throw error
+//   } else if (status === 'resolved') {
+//     return <PokemonDataView pokemon={pokemon} />
+//   }
+//
+//   throw new Error('This should be impossible')
+// }
+//
+// function ErrorFallback({error}) {
+//   return (
+//     <div role="alert">
+//       There was an error:{' '}
+//       <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+//     </div>
+//   )
+// }
+//
+// function App() {
+//   const [pokemonName, setPokemonName] = React.useState('')
+//
+//   function handleSubmit(newPokemonName) {
+//     setPokemonName(newPokemonName)
+//   }
+//
+//   return (
+//     <div className="pokemon-info-app">
+//       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+//       <hr />
+//       <div className="pokemon-info">
+//         <ErrorBoundary FallbackComponent={ErrorFallback} key={pokemonName}>
+//           <PokemonInfo pokemonName={pokemonName} />
+//         </ErrorBoundary>
+//       </div>
+//     </div>
+//   )
+// }
+//
+// export default App
 
-class ErrorBoundary extends React.Component {
-  state = {error: null}
-  static getDerivedStateFromError(error) {
-    return {error}
-  }
-  render() {
-    const {error} = this.state
-    if (error) {
-      return <this.props.FallbackComponent error={error} />
-    }
+//react-error-boundary library
 
-    return this.props.children
-  }
-}
+//
+// function PokemonInfo({pokemonName}) {
+//   const [state, setState] = React.useState({
+//     status: 'idle',
+//     pokemon: null,
+//     error: null,
+//   })
+//   const {status, pokemon, error} = state
+//
+//   React.useEffect(() => {
+//     if (!pokemonName) {
+//       return
+//     }
+//     setState({status: 'pending'})
+//     fetchPokemon(pokemonName).then(
+//       pokemon => {
+//         setState({status: 'resolved', pokemon})
+//       },
+//       error => {
+//         setState({status: 'rejected', error})
+//       },
+//     )
+//   }, [pokemonName])
+//
+//   if (status === 'idle') {
+//     return 'Submit a pokemon'
+//   } else if (status === 'pending') {
+//     return <PokemonInfoFallback name={pokemonName} />
+//   } else if (status === 'rejected') {
+//     // this will be handled by an error boundary
+//     throw error
+//   } else if (status === 'resolved') {
+//     return <PokemonDataView pokemon={pokemon} />
+//   }
+//
+//   throw new Error('This should be impossible')
+// }
+//
+// function ErrorFallback({error}) {
+//   return (
+//     <div role="alert">
+//       There was an error:{' '}
+//       <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+//     </div>
+//   )
+// }
+//
+// function App() {
+//   const [pokemonName, setPokemonName] = React.useState('')
+//
+//   function handleSubmit(newPokemonName) {
+//     setPokemonName(newPokemonName)
+//   }
+//
+//   return (
+//     <div className="pokemon-info-app">
+//       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+//       <hr />
+//       <div className="pokemon-info">
+//         <ErrorBoundary key={pokemonName} FallbackComponent={ErrorFallback}>
+//           <PokemonInfo pokemonName={pokemonName} />
+//         </ErrorBoundary>
+//       </div>
+//     </div>
+//   )
+// }
+//
+// export default App
+
+
+//error boundary reset
+
 
 function PokemonInfo({pokemonName}) {
   const [state, setState] = React.useState({
-    status: 'idle',
+    status: pokemonName ? 'pending' : 'idle',
     pokemon: null,
     error: null,
   })
@@ -251,11 +395,12 @@ function PokemonInfo({pokemonName}) {
   throw new Error('This should be impossible')
 }
 
-function ErrorFallback({error}) {
+function ErrorFallback({error, resetErrorBoundary}) {
   return (
     <div role="alert">
       There was an error:{' '}
       <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
     </div>
   )
 }
@@ -267,12 +412,16 @@ function App() {
     setPokemonName(newPokemonName)
   }
 
+  function handleReset() {
+    setPokemonName('')
+  }
+
   return (
     <div className="pokemon-info-app">
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <ErrorBoundary FallbackComponent={ErrorFallback} key={pokemonName}>
+        <ErrorBoundary FallbackComponent={ErrorFallback} onReset={handleReset}>
           <PokemonInfo pokemonName={pokemonName} />
         </ErrorBoundary>
       </div>
